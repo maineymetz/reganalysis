83c8cf24a5332a8e59967ad3b13ed3cfd813bf56
maineymetz <jmetz3@hotmail.co.uk>
2023-05-04 13:15:39 +0100

    Add code files

---
+++ script.js
@@ -0,0 +1,370 @@
+function navigateToUser() {
+    var dropdown = document.getElementById("user-dropdown");
+    var selectedUser = dropdown.value;
+    if (selectedUser !== "") {
+      window.location.href = selectedUser;
+    }
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box1");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 39) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 39 && value >= 30) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box2");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 50) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 50 && value >= 44) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box3");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 54) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 54 && value >= 43) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box4");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 56) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 56 && value >= 48) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box5");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 42) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 42 && value >= 30) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box6");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 62) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 62 && value >= 44) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box7");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 8) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 8 && value >= 5) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box8");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 8) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 8 && value >= 6) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box9");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 13) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 13 && value >= 9) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box10");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 46) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 46 && value >= 35) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box11");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 43) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 43 && value >= 36) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box12");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 83) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 83 && value >= 78) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box13");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 40) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 40 && value >= 28) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box14");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 85) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 85 && value >= 78) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box15");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 88) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 88 && value >= 76) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box16");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 39) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 39 && value >= 33) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box17");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 57) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 57 && value >= 46) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box18");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 47) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 47 && value >= 36) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box19");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 44) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 44 && value >= 37) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box20");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 49) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 49 && value >= 40) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box21");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 58) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 58 && value >= 51) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box22");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 68) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 68 && value >= 59) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box23");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 57) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 57 && value >= 49) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box24");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 50) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 50 && value >= 36) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box25");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 48) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 48 && value >= 43) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box26");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 20) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 20 && value >= 15) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box27");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 40) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 40 && value >= 32) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+  document.addEventListener("DOMContentLoaded", function() {
+    var box1 = document.getElementById("box28");
+    var value = parseInt(box1.textContent);
+  
+    if (value > 53) {
+      box1.parentElement.style.backgroundColor = "red";
+    } else if (value <= 53 && value >= 47) {
+      box1.parentElement.style.backgroundColor = "yellow";
+    } else {
+      box1.parentElement.style.backgroundColor = "green";
+    }
+  });
+
+                                        
+  // Get the container elements that hold the boxes
+var containerRows = document.querySelectorAll('.box-row');
+
+// Define the desired order of the box IDs in rows
+var desiredOrderRows = [
+  ['box1', 'box2', 'box3', 'box4', 'box5', 'box6', 'box24'],
+  ['box7', 'box8', 'box9', 'box10', 'box11', 'box12', 'box25'],
+  ['box13', 'box14', 'box15', 'box26', 'box27', 'box16', 'box17'],
+  ['box18', 'box22', 'box19', 'box20', 'box21', 'box28'],
+  ['box23']
+];
+
+// Rearrange the boxes in rows based on the desired order
+containerRows.forEach(function(container, rowIndex) {
+  desiredOrderRows[rowIndex].forEach(function(boxId) {
+    var box = document.getElementById(boxId);
+    container.appendChild(box.parentNode.parentNode); // Move the entire box element
+  });
+});
+
+     
+        
+        
